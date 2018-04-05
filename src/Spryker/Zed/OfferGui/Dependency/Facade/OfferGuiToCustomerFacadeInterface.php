2edcdf0556035a414fc238ccfb171919d6f2ba34
Sergey Sikachev <sergey.sikachev@spryker.com>
2018-04-05 19:59:15 +0300

    CORE-3784. Partially fix todos for OfferGui and Offer modules.

--- src/Spryker/Zed/OfferGui/Dependency/Facade/OfferGuiToCustomerFacadeInterface.php
+++ src/Spryker/Zed/OfferGui/Dependency/Facade/OfferGuiToCustomerFacadeInterface.php
@@ -7,6 +7,7 @@
 
 namespace Spryker\Zed\OfferGui\Dependency\Facade;
 
+use Generated\Shared\Transfer\CustomerCollectionTransfer;
 use Generated\Shared\Transfer\CustomerTransfer;
 
 interface OfferGuiToCustomerFacadeInterface
@@ -17,4 +18,11 @@ interface OfferGuiToCustomerFacadeInterface
      * @return \Generated\Shared\Transfer\CustomerTransfer|null
      */
     public function findCustomerByReference($customerReference): ?CustomerTransfer;
+
+    /**
+     * @param \Generated\Shared\Transfer\CustomerCollectionTransfer $customerCollectionTransfer
+     *
+     * @return \Generated\Shared\Transfer\CustomerCollectionTransfer
+     */
+    public function getCustomerCollection(CustomerCollectionTransfer $customerCollectionTransfer): CustomerCollectionTransfer;
 }
--- src/Spryker/Zed/OfferGui/OfferGuiConfig.php
+++ src/Spryker/Zed/OfferGui/OfferGuiConfig.php
@@ -11,16 +11,4 @@ use Spryker\Zed\Kernel\AbstractBundleConfig;
 
 class OfferGuiConfig extends AbstractBundleConfig
 {
-    /**
-     * @uses \Spryker\Zed\Offer\OfferConfig::getOrderTypeOffer()
-     */
-    public const ORDER_TYPE_OFFER = 'offer';
-
-    /**
-     * @return string
-     */
-    public function getOrderTypeOffer(): string
-    {
-        return static::ORDER_TYPE_OFFER;
-    }
 }
